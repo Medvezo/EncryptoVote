85dd39f2ca7aaeb7e021a465433c1bbf874ab525
Medvezo <mansur.mustafayev@gmail.com>
2024-03-07 10:58:22 +0400

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Encrypto Vote",
+  description: "Decentralized Voting system in Web 3.0 for companies and goverments",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center flex-col min-h-screen"></main>
+  );
+}
