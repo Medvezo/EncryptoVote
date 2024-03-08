7021c2b9785dda4c7fdaa6213386eab6a482d7f9
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-03-08 20:12:17 +0400

    feat: add Bento Grid to Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
 import { MacbookScroll } from "@/components/ui/macbook-scroll";
 import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
 
+import { BentoGrid, BentoGridItem } from "@/components/ui/bento-grid";
+import {
+	IconArrowWaveRightUp,
+	IconBoxAlignRightFilled,
+	IconBoxAlignTopLeft,
+	IconClipboardCopy,
+	IconFileBroken,
+	IconSignature,
+	IconTableColumn,
+} from "@tabler/icons-react";
+
 export default function Home() {
 	const headerWords = [
 		{
@@ -14,24 +25,82 @@ export default function Home() {
 			className: "text-blue-500 dark:text-blue-500",
 		},
 	];
-	
+
 	return (
 		<main className="flex flex-col justify-center items-center min-h-screen">
-
 			{/* TypeWriter Header */}
 			<div className="flex flex-col items-center justify-center h-[50vh]  ">
 				<p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
-					The road to freedom starts from here
+					The road to <b className="text-blue-800">justice</b> starts from here
 				</p>
 				<TypewriterEffectSmooth words={headerWords} />
 			</div>
-
 			<MacbookScroll
 				title={<span className="text-white">Vote from your Home!</span>}
 				src={`/linear.webp`}
 				showGradient={false}
 			/>
-			<div className="min-h-[400vh]">a</div>
+			<BentoGrid className="max-w-4xl mx-auto">
+				{items.map((item, i) => (
+					<BentoGridItem
+						key={i}
+						title={item.title}
+						description={item.description}
+						header={item.header}
+						icon={item.icon}
+						className={i === 3 || i === 6 ? "md:col-span-2" : ""}
+					/>
+				))}
+			</BentoGrid>{" "}
 		</main>
 	);
 }
+const Skeleton = () => (
+	<div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
+);
+
+const items = [
+	{
+		title: "The Dawn of Innovation",
+		description: "Explore the birth of groundbreaking ideas and inventions.",
+		header: <Skeleton />,
+		icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
+	},
+	{
+		title: "The Digital Revolution",
+		description: "Dive into the transformative power of technology.",
+		header: <Skeleton />,
+		icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
+	},
+	{
+		title: "The Art of Design",
+		description: "Discover the beauty of thoughtful and functional design.",
+		header: <Skeleton />,
+		icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
+	},
+	{
+		title: "The Power of Communication",
+		description:
+			"Understand the impact of effective communication in our lives.",
+		header: <Skeleton />,
+		icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
+	},
+	{
+		title: "The Pursuit of Knowledge",
+		description: "Join the quest for understanding and enlightenment.",
+		header: <Skeleton />,
+		icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
+	},
+	{
+		title: "The Joy of Creation",
+		description: "Experience the thrill of bringing ideas to life.",
+		header: <Skeleton />,
+		icon: <IconBoxAlignTopLeft className="h-4 w-4 text-neutral-500" />,
+	},
+	{
+		title: "The Spirit of Adventure",
+		description: "Embark on exciting journeys and thrilling discoveries.",
+		header: <Skeleton />,
+		icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
+	},
+];
