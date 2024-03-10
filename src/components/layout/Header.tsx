25bc488289e06bb0e870f402b34fd707bbdfaed9
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-03-10 15:54:00 +0400

    feat: Add Header container component

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+	return (
+		<div className="w-full h-16 fixed top-0 shadow-lg shadow-blue-950/50 bg-[#03001417] backdrop-blur-md z-50 lg:px-10">
+			<div className="w-full h-full flex flex-row items-center justify-between m-auto px-3 overflow-hidden"></div>
+		</div>
+	);
+}
