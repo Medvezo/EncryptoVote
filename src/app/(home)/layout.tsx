fdc10ea5cb4cdb0b38ac787dd27df67dd4bfdec6
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-03-10 15:56:24 +0400

    refactor: Create (home) group route w its layout n page

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,15 @@
+import Header from "@/components/layout/Header";
+
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+        <>
+            <Header />
+			{children}
+        </>
+	);
+}
