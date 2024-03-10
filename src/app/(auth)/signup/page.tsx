e082d992afe17ae3f7d1e6396d5581eacdcaf74d
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-03-10 16:40:53 +0400

    feat: add login n signup in (auth) group route with form

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,9 @@
+import SignupForm from "@/components/container/SignUpForm";
+
+export default function SignUpPage() {
+	return (
+		<main className="flex justify-center items-center h-screen">
+			<SignupForm />
+		</main>
+	);
+}
