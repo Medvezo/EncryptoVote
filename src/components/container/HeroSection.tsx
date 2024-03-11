fb324e3149e05a993aef5c1b905c733a1f0ed286
Medvezo <mansur.mustafayev@gmail.com>
2024-03-11 13:07:23 +0400

    feat: add CTA Buttons to HeroSection

--- src/components/container/HeroSection.tsx
+++ src/components/container/HeroSection.tsx
@@ -1,4 +1,6 @@
 import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function HeroSection() {
 	const headerWords = [
@@ -15,11 +17,21 @@ export default function HeroSection() {
 	];
 
 	return (
-		<section className="flex flex-col items-center justify-center h-[50vh]  ">
-			<p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
-				The road to <b className="text-blue-800">justice</b> starts from here
-			</p>
-			<TypewriterEffectSmooth words={headerWords} />
+		<section className="flex flex-col gap-20 items-center justify-center mt-24  ">
+			<div className="flex justify-center items-center flex-col">
+				<p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
+					The road to <b className="text-blue-800">justice</b> starts from here
+				</p>
+				<TypewriterEffectSmooth words={headerWords} />
+			</div>
+			<div className="flex gap-10  ">
+				<Button variant={"accent"} asChild>
+					<Link href={"/action"}> Vote </Link>
+				</Button>
+				<Button variant={"secondary"} asChild>
+					<Link href={"#bentoGridSection"}>Learn More</Link>
+				</Button>
+			</div>
 		</section>
 	);
 }
