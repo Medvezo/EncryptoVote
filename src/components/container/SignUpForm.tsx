9c58c8f7909fe160a9b7e5bc5f866bf7c76e37fa
Medvezo <mansur.mustafayev@gmail.com>
2024-03-11 08:51:36 +0400

    refactor: segregate BottomGradient component from SignupForm

--- src/components/container/SignUpForm.tsx
+++ src/components/container/SignUpForm.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { Label } from "@/components/ui/AceLabel";
 import { Input } from "@/components/ui/AceInput";
 import { cn } from "@/utils/cn";
+import { BottomGradient } from "@/components/common/BottomGradient";
 
 export default function SignupForm() {
 	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -49,8 +50,7 @@ export default function SignupForm() {
 
 				<button
 					className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-					type="submit"
-				>
+					type="submit">
 					Sign up &rarr;
 					<BottomGradient />
 				</button>
@@ -60,15 +60,6 @@ export default function SignupForm() {
 		</div>
 	);
 }
-
-const BottomGradient = () => {
-	return (
-		<>
-			<span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
-			<span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
-		</>
-	);
-};
 
 const LabelInputContainer = ({
 	children,
