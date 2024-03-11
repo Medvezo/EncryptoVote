151e6b3dd84e985802f33b8f9ab83ad3c7c0f4da
Medvezo <mansur.mustafayev@gmail.com>
2024-03-11 13:19:56 +0400

    styling: refine styling of action page

--- src/app/(auth)/action/page.tsx
+++ src/app/(auth)/action/page.tsx
@@ -2,16 +2,26 @@ import { DirectionAwareHover } from "@/components/ui/direction-aware-hover";
 
 export default function page() {
 	return (
-		<main className="">
-            <header >
-                <h2 className="text-center text-4xl py-10 text-blue-500 font-bold">Choose your side</h2>
-            </header>
-			<section className="flex gap-5 px-5">
-				<DirectionAwareHover imageUrl="/CompanyVotingHands.jpg">
-					<p>Company</p>
+		<main className="flex justify-center items-center flex-col h-screen gap-16">
+			<header>
+				<h2 className="text-center text-4xl text-blue-500 font-bold">
+					Sign in for
+				</h2>
+			</header>
+			<section className="flex gap-5 px-5 ">
+				<DirectionAwareHover
+					childrenClassName="flex justify-center items-end h-full w-full"
+					imageUrl="/CompanyVotingHands.jpg">
+					<p className="text-3xl font-bold bg-black/50 rounded-2xl p-1 text-amber-500">
+						Company
+					</p>
 				</DirectionAwareHover>
-				<DirectionAwareHover imageUrl="/SingleVoterHand.png">
-					<p>Voter</p>
+				<DirectionAwareHover
+					childrenClassName="flex justify-center items-end h-full w-full"
+					imageUrl="/SingleVoterHand.png">
+					<p className="text-3xl font-bold bg-black/50 rounded-2xl p-1 text-amber-500">
+						Voter
+					</p>
 				</DirectionAwareHover>
 			</section>
 		</main>
