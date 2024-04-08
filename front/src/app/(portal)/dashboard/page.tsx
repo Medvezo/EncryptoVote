9c14a7b988ca4276f9513b170ea559ed55784a9a
Medvezo <mansur.mustafayev@gmail.com>
2024-04-08 11:24:44 +0400

    refactor: delete Tabs func in main /dashboard

--- front/src/app/(portal)/dashboard/page.tsx
+++ front/src/app/(portal)/dashboard/page.tsx
@@ -53,7 +53,6 @@ import {
 	TableHeader,
 	TableRow,
 } from "@/components/ui/table";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
 	Tooltip,
 	TooltipContent,
@@ -247,16 +246,7 @@ export default function Dashboard() {
 					</DropdownMenu>
 				</header>
 				<main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-					<Tabs defaultValue="all">
 						<div className="flex items-center">
-							<TabsList>
-								<TabsTrigger value="all">All</TabsTrigger>
-								<TabsTrigger value="active">Active</TabsTrigger>
-								<TabsTrigger value="draft">Draft</TabsTrigger>
-								<TabsTrigger value="archived" className="hidden sm:flex">
-									Archived
-								</TabsTrigger>
-							</TabsList>
 							<div className="ml-auto flex items-center gap-2">
 								<DropdownMenu>
 									<DropdownMenuTrigger asChild>
@@ -279,12 +269,7 @@ export default function Dashboard() {
 										</DropdownMenuCheckboxItem>
 									</DropdownMenuContent>
 								</DropdownMenu>
-								<Button size="sm" variant="outline" className="h-8 gap-1">
-									<File className="h-3.5 w-3.5" />
-									<span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-										Export
-									</span>
-								</Button>
+								
 								<Button size="sm" className="h-8 gap-1">
 									<PlusCircle className="h-3.5 w-3.5" />
 									<span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
@@ -293,7 +278,7 @@ export default function Dashboard() {
 								</Button>
 							</div>
 						</div>
-						<TabsContent value="all">
+						<section>
 							<Card x-chunk="dashboard-06-chunk-0">
 								<CardHeader>
 									<CardTitle>Products</CardTitle>
@@ -599,8 +584,7 @@ export default function Dashboard() {
 									</div>
 								</CardFooter>
 							</Card>
-						</TabsContent>
-					</Tabs>
+						</section>
 				</main>
 			</div>
 		</div>
