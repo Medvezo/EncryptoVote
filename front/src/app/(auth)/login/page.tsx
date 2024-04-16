2e402821ccbacfc32a6aa25e3f50ddad3039ebd4
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-04-16 04:47:59 +0400

    [front] refactor: change correct component render in login page

--- front/src/app/(auth)/login/page.tsx
+++ front/src/app/(auth)/login/page.tsx
@@ -1,9 +1,9 @@
-import SignupForm from "@/components/container/SignUpForm";
+import LoginForm from "@/components/container/LoginForm";
 
 export default function SignUpPage() {
 	return (
 		<main className="flex justify-center items-center h-screen">
-			<SignupForm />
+			<LoginForm />
 		</main>
 	);
 }
