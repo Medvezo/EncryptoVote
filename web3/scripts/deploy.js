9bdfe009691ea17b1d1c5b0eb5a46a38818f81b3
Medvezo <mansur.mustafayev@gmail.com>
2024-04-25 09:43:54 +0400

    [web3] refactor: replace ignition w scripts folder

---
+++ web3/scripts/deploy.js
@@ -0,0 +1,17 @@
+async function main() {
+	const [deployer] = await ethers.getSigners();
+	console.log("Deploying contracts with the account:", deployer.address);
+
+	const Contract = await ethers.getContractFactory("MyContract");
+	const contract = await Contract.deploy();
+	await contract.deployed();
+
+	console.log("Contract deployed to:", contract.address);
+}
+
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});
