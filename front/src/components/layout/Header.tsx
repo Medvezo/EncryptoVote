e6d342b43a0da25870cd1846308be1955c9d6ca6
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-04-27 21:12:48 +0400

    [front] feat: add Logo to Header comp

--- front/src/components/layout/Header.tsx
+++ front/src/components/layout/Header.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
+
 export default function Header() {
 	return (
-		<div className="w-full h-16 fixed top-0 shadow-lg shadow-blue-950/50 bg-[#03001417] backdrop-blur-md z-50 lg:px-10">
-			<div className="w-full h-full flex flex-row items-center justify-between m-auto px-3 overflow-hidden"></div>
+		<div className="w-full h-20 fixed top-0 shadow-lg shadow-blue-950/50 bg-[#03001417] backdrop-blur-md z-50 lg:px-10">
+			<div className="w-full h-full flex flex-row items-center justify-between m-auto px-3 overflow-hidden">
+				<div className="flex flex-row justify-center items-center gap-5">
+					<Image
+						src={"/Logo.webp"}
+						alt="EncryptoVote logo"
+						width={70}
+						height={70}
+						className="rounded-full  "
+					/>
+					<h1 className="text-xl sm:text-2xl lg:text-3xl">EncryptoVote</h1>
+				</div>
+			</div>
 		</div>
 	);
 }
