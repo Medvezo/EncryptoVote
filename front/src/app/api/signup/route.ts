822b32adc52801b08cd3a62740528fefc561e72d
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-04-27 21:22:08 +0400

    [front] refactor: change api/signup similar to /login

--- front/src/app/api/signup/route.ts
+++ front/src/app/api/signup/route.ts
@@ -1,23 +1,39 @@
-// app/api/signup.ts
-import { NextApiRequest, NextApiResponse } from "next";
-import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
 import axiosBase from "@/helpers/axiosBase";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method !== "POST") {
-    return res.status(405).end(); // Method Not Allowed
-  }
+// The POST function is defined to handle POST requests specifically.
+export async function POST(request: NextRequest) {
+	// Check if the method is POST, otherwise return method not allowed.
+	if (request.method !== "POST") {
+		return new NextResponse(null, { status: 405 });
+	}
+	console.log("Reached /api/signup");
 
-  try {
-    const response = await axiosBase.post(
-      "/signup",
-      req.body
-    );
-    return res.status(response.status).json(response.data);
-  } catch (error: any) {
-    return res.status(error.response.status).json(error.response.data);
-  }
+	try {
+		// Extract the request body
+		const reqBody = await request.json();
+
+		// Make the POST request to the Laravel backend with axios to create a new user.
+		const response = await axiosBase.post("/signup", reqBody);
+		console.log("Signup request sent", reqBody);
+
+		// Create a new NextResponse object with the response data and status.
+		return new NextResponse(JSON.stringify(response.data), {
+			status: response.status,
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+	} catch (error: any) {
+		// If an error occurs, parse the error response and send it back.
+		const status = error.response?.status || 500;
+		const data = error.response?.data || { error: "An error occurred" };
+
+		return new NextResponse(JSON.stringify(data), {
+			status,
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+	}
 }
