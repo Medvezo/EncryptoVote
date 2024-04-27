2ce1a3da28ce3c9aee86eade7e43ddbc91ce2d73
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-04-27 21:18:59 +0400

    [front] refactor: change font from Inter to Oxanium

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Oxanium } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const oxanium = Oxanium({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Encrypto Vote",
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en" className="dark">
-			<body className={inter.className}>{children}</body>
+			<body className={oxanium.className}>{children}</body>
 		</html>
 	);
 }
