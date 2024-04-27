cf0d4642237b08ebd7671d6fca6128e7cb777f5a
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-04-27 22:06:46 +0400

    [front] refactor: make text interactive on hover n add gradient

--- front/src/components/layout/Header.tsx
+++ front/src/components/layout/Header.tsx
@@ -8,18 +8,23 @@ export default function Header() {
 			<div className="w-full h-full flex flex-row items-center justify-between m-auto px-3 overflow-hidden">
 				<Link
 					href={"/"}
-					className="flex flex-row justify-center items-center gap-5"
+					className=" relative flex flex-row justify-center items-center gap-5 group"
 				>
 					<Image
 						src={"/Logo.webp"}
 						alt="EncryptoVote logo"
 						width={70}
 						height={70}
-						className="rounded-full  "
+						className="rounded-full"
 					/>
-					<h1 className="text-xl sm:text-2xl lg:text-3xl">EncryptoVote</h1>
+					<h1 className="text-xl sm:text-2xl lg:text-3xl ">
+						Encrypto
+						<span className="absolute top-1/2 -translate-y-1/2 group-hover:text-4xl text-transparent bg-clip-text bg-gradient-to-l from-blue-500 to-cyan-300 transition-all duration-300 ease-out">
+							Vote
+						</span>
+					</h1>
 				</Link>
-				
+
 				<Button variant={"accent"} asChild>
 					<Link href={"/login"} className="font-semibold text-xl">
 						{" "}
