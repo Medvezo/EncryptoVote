b7461c674071239730d1d0c7196d47067216f3f1
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-04-27 21:36:47 +0400

    [front] feat: add buttons w links to Header comp

--- front/src/components/layout/Header.tsx
+++ front/src/components/layout/Header.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
+import { Button } from "../ui/button";
+import Link from "next/link";
 
 export default function Header() {
 	return (
 		<div className="w-full h-20 fixed top-0 shadow-lg shadow-blue-950/50 bg-[#03001417] backdrop-blur-md z-50 lg:px-10">
 			<div className="w-full h-full flex flex-row items-center justify-between m-auto px-3 overflow-hidden">
-				<div className="flex flex-row justify-center items-center gap-5">
+				<Link
+					href={"/"}
+					className="flex flex-row justify-center items-center gap-5"
+				>
 					<Image
 						src={"/Logo.webp"}
 						alt="EncryptoVote logo"
@@ -13,7 +18,14 @@ export default function Header() {
 						className="rounded-full  "
 					/>
 					<h1 className="text-xl sm:text-2xl lg:text-3xl">EncryptoVote</h1>
-				</div>
+				</Link>
+				
+				<Button variant={"accent"} asChild>
+					<Link href={"/login"} className="font-semibold text-xl">
+						{" "}
+						Make Vote{" "}
+					</Link>
+				</Button>
 			</div>
 		</div>
 	);
