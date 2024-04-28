2d4522730733ad1bc0537731c182716dcddf7475
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-04-28 18:20:24 +0400

    [front] style: improve styling in CryptoAythButton

--- front/src/components/container/CryptoAuthButton.tsx
+++ front/src/components/container/CryptoAuthButton.tsx
@@ -5,41 +5,42 @@ import { ethers } from "ethers"; // Ensure ethers is properly imported
 import { useState } from "react";
 import { randomString } from "@/helpers/random";
 
-
 export default function CryptoAuthButton() {
-    const [message, setMessage] = useState("");
-    const [account, setAccount] = useState("");
+	const [message, setMessage] = useState("");
+	const [account, setAccount] = useState("");
 
-    const handleCryptoButton = async () => {
-        const { ethereum } = window as any;
-        if (ethereum && ethereum.isMetaMask) {
-            setMessage("MetaMask Installed");
-            await ethereum.request({ method: "eth_requestAccounts" });
-            const accounts = await ethereum.request({ method: "eth_accounts" });
+	const handleCryptoButton = async () => {
+		const { ethereum } = window as any;
+		if (ethereum && ethereum.isMetaMask) {
+			setMessage("MetaMask Installed");
+			await ethereum.request({ method: "eth_requestAccounts" });
+			const accounts = await ethereum.request({ method: "eth_accounts" });
 
-            const provider = new ethers.providers.Web3Provider(ethereum); // Use Web3Provider for ethers v6
-            const signer = provider.getSigner();
-            const randomMsg = randomString(16); // Avoid reusing `message` variable
-            const signature = await signer.signMessage(randomMsg);
+			const provider = new ethers.providers.Web3Provider(ethereum); // Use Web3Provider for ethers v6
+			const signer = provider.getSigner();
+			const randomMsg = randomString(16); // Avoid reusing `message` variable
+			const signature = await signer.signMessage(randomMsg);
 
-            const signAddress = await ethers.utils.verifyMessage(randomMsg, signature);
-            if (signAddress.toLowerCase() === accounts[0].toLowerCase()) {
-                setMessage("User Login");
-                setAccount(accounts[0]);
-            } else {
-                setMessage("Login failed");
-            }
-        } else {
-            setMessage("MetaMask is not installed");
-        }
-    };
+			const signAddress = await ethers.utils.verifyMessage(
+				randomMsg,
+				signature
+			);
+			if (signAddress.toLowerCase() === accounts[0].toLowerCase()) {
+				setMessage("User Login");
+				setAccount(accounts[0]);
+			} else {
+				setMessage("Login failed");
+			}
+		} else {
+			setMessage("MetaMask is not installed");
+		}
+	};
 
-    return (
-        <div>
-            <h3>Crypto Authentication</h3>
-            <Button onClick={handleCryptoButton}>Connect Wallet</Button>
-            <p>{message}</p>
-            {message === "User Login" && <div>Account: {account}</div>}
-        </div>
-    );
+	return (
+		<div className="flex justify-center items-center my-3 mt-5">
+			<Button onClick={handleCryptoButton} variant="default" className="font-bold text-lg">Connect Wallet</Button>
+			<p>{message}</p>
+			{message === "User Login" && <div>Account: {account}</div>}
+		</div>
+	);
 }
