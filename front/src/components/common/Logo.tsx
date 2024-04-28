41cea17eb2832567ba74e7bfa11ddc1d17b39f01
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-04-28 16:23:44 +0400

    [front] refactor: improve component funcs of Logo

--- front/src/components/common/Logo.tsx
+++ front/src/components/common/Logo.tsx
@@ -3,28 +3,32 @@ import Image from "next/image";
 
 type TLogo = {
 	size: number;
-	direction: "row" | "col";
+	noText?: boolean;
+    href: string;
 };
 
-export default function Logo({ size, direction }: TLogo) {
+export default function Logo({ size, noText, href }: TLogo) {
 	return (
 		<Link
-			href={"/"}
-			className={`relative flex flex-${direction} justify-center items-center gap-5 group`}
+			href={`${href}`}
+			className={`relative flex flex-row justify-center items-center gap-5 group`}
 		>
 			<Image
 				src={"/Logo.webp"}
 				alt="EncryptoVote logo"
 				width={size}
 				height={size}
-				className="rounded-full"
+				className="rounded-full min-w-16 min-h-16"
 			/>
-			<h1 className="text-xl sm:text-2xl lg:text-3xl ">
-				Encrypto
-				<span className="absolute top-1/2 -translate-y-1/2 group-hover:text-4xl text-transparent bg-clip-text bg-gradient-to-l from-blue-500 to-cyan-300 transition-all duration-300 ease-out">
-					Vote
-				</span>
-			</h1>
+			{!noText && (
+				<h1 className="text-xl sm:text-2xl lg:text-3xl ">
+					Encrypto
+					<span className="absolute top-1/2 -translate-y-1/2 group-hover:text-4xl text-transparent bg-clip-text bg-gradient-to-l from-blue-500 to-cyan-300 transition-all duration-300 ease-out">
+						Vote
+					</span>
+				</h1>
+			)}
+            
 		</Link>
 	);
 }
