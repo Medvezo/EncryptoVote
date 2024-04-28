f4c29231e3206fce1bcac47b1dbf4102890de997
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-04-28 21:05:21 +0400

    [front] feat: error n promise handling w toasts in CryptoAuthButton

--- front/src/components/container/CryptoAuthButton.tsx
+++ front/src/components/container/CryptoAuthButton.tsx
@@ -1,46 +1,63 @@
 "use client";
 
 import { Button } from "../ui/button";
-import { ethers } from "ethers"; // Ensure ethers is properly imported
+import { ethers } from "ethers";
 import { useState } from "react";
 import { randomString } from "@/helpers/random";
+import { toast } from "react-toastify";
 
 export default function CryptoAuthButton() {
-	const [message, setMessage] = useState("");
-	const [account, setAccount] = useState("");
+	const [account, setAccount] = useState<string | null>(null);
 
 	const handleCryptoButton = async () => {
 		const { ethereum } = window as any;
 		if (ethereum && ethereum.isMetaMask) {
-			setMessage("MetaMask Installed");
 			await ethereum.request({ method: "eth_requestAccounts" });
+
 			const accounts = await ethereum.request({ method: "eth_accounts" });
-
-			const provider = new ethers.providers.Web3Provider(ethereum); // Use Web3Provider for ethers v6
+			const provider = new ethers.providers.Web3Provider(ethereum);
 			const signer = provider.getSigner();
-			const randomMsg = randomString(16); // Avoid reusing `message` variable
-			const signature = await signer.signMessage(randomMsg);
+			const randomMsg = randomString(16);
 
-			const signAddress = await ethers.utils.verifyMessage(
-				randomMsg,
-				signature
+			toast.promise(
+				new Promise(async (resolve, reject) => {
+					try {
+						const signature = await signer.signMessage(randomMsg);
+						const signAddress = await ethers.utils.verifyMessage(
+							randomMsg,
+							signature
+						);
+
+						if (signAddress.toLowerCase() === accounts[0].toLowerCase()) {
+							setAccount(accounts[0]);
+							resolve("User successfully logged in.");
+						} else {
+							reject("Failed to login.");
+						}
+					} catch (error) {
+						reject("Signing failed.");
+					}
+				}),
+				{
+					pending: "Signing wallet...",
+					success: "Signing successful! 🎉",
+					error: "Signing failed 🤯",
+				}
 			);
-			if (signAddress.toLowerCase() === accounts[0].toLowerCase()) {
-				setMessage("User Login");
-				setAccount(accounts[0]);
-			} else {
-				setMessage("Login failed");
-			}
 		} else {
-			setMessage("MetaMask is not installed");
+			toast.info("Install one of our supported wallets");
 		}
 	};
 
 	return (
 		<div className="flex flex-col justify-center items-center my-3 mt-10">
-			<Button onClick={handleCryptoButton} variant="default" className="font-bold text-lg">Connect Wallet</Button>
-			<p>{message}</p>
-			{message === "User Login" && <div>Account: {account}</div>}
+			<Button
+				onClick={handleCryptoButton}
+				variant="default"
+				className="font-bold text-lg"
+			>
+				Connect Wallet
+			</Button>
 		</div>
 	);
 }
