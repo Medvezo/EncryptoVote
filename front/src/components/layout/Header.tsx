16924be49b60654cc6721c9c7a7f07ca50cb3601
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-04-28 16:14:37 +0400

    [front] refactor: replace Header w Logo

--- front/src/components/layout/Header.tsx
+++ front/src/components/layout/Header.tsx
@@ -1,29 +1,12 @@
-import Image from "next/image";
 import { Button } from "../ui/button";
 import Link from "next/link";
+import Logo from "../common/Logo";
 
 export default function Header() {
 	return (
 		<div className="w-full h-20 fixed top-0 shadow-lg shadow-blue-950/50 bg-[#03001417] backdrop-blur-md z-50 lg:px-10">
 			<div className="w-full h-full flex flex-row items-center justify-between m-auto px-3 overflow-hidden">
-				<Link
-					href={"/"}
-					className=" relative flex flex-row justify-center items-center gap-5 group"
-				>
-					<Image
-						src={"/Logo.webp"}
-						alt="EncryptoVote logo"
-						width={70}
-						height={70}
-						className="rounded-full"
-					/>
-					<h1 className="text-xl sm:text-2xl lg:text-3xl ">
-						Encrypto
-						<span className="absolute top-1/2 -translate-y-1/2 group-hover:text-4xl text-transparent bg-clip-text bg-gradient-to-l from-blue-500 to-cyan-300 transition-all duration-300 ease-out">
-							Vote
-						</span>
-					</h1>
-				</Link>
+				<Logo size={70} direction="row"/>
 
 				<Button variant={"accent"} asChild>
 					<Link href={"/login"} className="font-semibold text-xl">
