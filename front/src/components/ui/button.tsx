f0389a7f51f617e54748f5a05b329d717f2d9110
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-04-28 18:21:36 +0400

    [front] style: change color of button ui component in accent variation

--- front/src/components/ui/button.tsx
+++ front/src/components/ui/button.tsx
@@ -20,7 +20,7 @@ const buttonVariants = cva(
 				link: "text-primary underline-offset-4 hover:underline",
 
                 // Custom CTA
-                accent: "bg-primary-orange text-black   hover:bg-amber-600"
+                accent: "bg-amber-500 text-black   hover:bg-amber-600"
 			},
 			size: {
 				default: "h-10 px-4 py-2",
