ad075fab6c29082c136597bd1f6311c25b7712e6
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-04-30 21:22:06 +0400

    [front] feat: add loading.tsx for login page

---
+++ front/src/app/(auth)/login/loading.tsx
@@ -0,0 +1,33 @@
+import TwinkleSpikes from "@/components/common/TwinkleSpikes";
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function LoadingLoginForm() {
+	return (
+		<main className="flex justify-center items-center h-screen">
+			<section className=" w-full mx-auto flex flex-col items-center gap-10 p-4 md:p-8">
+				<aside className="flex justify-center items-center gap-5 w-full">
+					<Skeleton className="h-24 w-24 rounded-full" />
+					<Skeleton className="h-4 w-1/6 mb-2" />
+				</aside>
+				<div className="max-w-md mx-auto rounded-md shadow-input bg-white dark:bg-blue-950 p-4 md:p-8 w-full">
+					<Skeleton className="h-6 w-3/4 mb-4" />
+					<Skeleton className="h-4 w-11/12 mb-8" />
+					<form className="space-y-6 w-full">
+						<div>
+							<Skeleton className="h-4 w-1/4 mb-2" />
+							<Skeleton className="h-10 w-full rounded-md" />
+						</div>
+						<div>
+							<Skeleton className="h-4 w-1/4 mb-2" />
+							<Skeleton className="h-10 w-full rounded-md" />
+						</div>
+						<Skeleton className="h-10 w-full rounded-md" />
+					</form>
+					<Skeleton className="h-1 w-full bg-gray-300 dark:bg-gray-700 my-8" />
+					<Skeleton className="h-4 w-1/2" />
+				</div>
+			</section>
+			<TwinkleSpikes />
+		</main>
+	);
+}
