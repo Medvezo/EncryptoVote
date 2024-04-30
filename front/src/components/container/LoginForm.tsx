82819d5190d7699eb0d6890df17eac9292e6559c
GasimovNijat <nijatt.gasimov@gmail.com>
2024-04-30 13:00:14 +0400

    [front] fix: assign state to Inputs of login

--- front/src/components/container/LoginForm.tsx
+++ front/src/components/container/LoginForm.tsx
@@ -9,10 +9,10 @@ export default function LoginForm() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleSubmit = async (e) => {
+    const handleSubmit = async (e:any) => {
         e.preventDefault();
         try {
-            const data = await login({ email, password });
+            const data = await login(email , password);
             console.log('Login successful', data);
             // Handle successful login (e.g., redirect user to dashboard)
         } catch (error) {
@@ -32,11 +32,11 @@ export default function LoginForm() {
             <form onSubmit={handleSubmit}>
                 <LabelInputContainer>
                     <Label htmlFor="email">Email Address</Label>
-                    <Input id="email" placeholder="Email" type="email" />
+                    <Input id="email" placeholder="Email" type="email" value={email} onChange={(e:any) => setEmail(e.target.value)}/>
                 </LabelInputContainer>
                 <LabelInputContainer>
                     <Label htmlFor="password">Password</Label>
-                    <Input id="password" placeholder="Password" type="password" />
+                    <Input id="password" placeholder="Password" type="password" value={password}  onChange={(e:any) => setPassword(e.target.value)}/>
                 </LabelInputContainer>
                 <button
                     className="bg-gradient-to-br relative my-5 group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
