b768a58bc829eb4a11d95a70cc907269a23089c1
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-05-02 21:44:12 +0400

    [front] feat: add state for textarea in ContactForm

--- front/src/components/container/ContactForm.tsx
+++ front/src/components/container/ContactForm.tsx
@@ -9,6 +9,7 @@ import { useState } from "react";
 export default function ContactForm() {
 	const [email, setEmail] = useState("");
 	const [name, setName] = useState("");
+	const [message, setMessage] = useState("");
 
 	return (
 		<form
@@ -45,21 +46,20 @@ export default function ContactForm() {
 						</LabelInputContainer>
 					</div>
 					<div className="sm:col-span-2">
-						<label
-							htmlFor="message"
-							className="block text-sm font-semibold leading-6 text-white"
-						>
-							Message
-						</label>
-						<div className="mt-2.5">
-							<textarea
-								name="message"
-								id="message"
-								rows={4}
-								className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-4 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
-								defaultValue={""}
-							/>
-						</div>
+						<LabelInputContainer>
+							<Label htmlFor="message">Message</Label>
+							<div className="mt-2.5">
+								<textarea
+									name="message"
+									id="message"
+									rows={4}
+									className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-4 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
+									defaultValue={""}
+                                    value={message}
+									onChange={(e: any) => setMessage(e.target.value)}
+								/>
+							</div>
+						</LabelInputContainer>
 					</div>
 				</div>
 				<div className="mt-8 flex justify-end">
