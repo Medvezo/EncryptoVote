c8bfbfc4f3d14df088e62272144ab4878e5390d0
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-05-02 20:44:34 +0400

    [front] style: replace footer mt w pb of page in /home

--- front/src/app/(home)/page.tsx
+++ front/src/app/(home)/page.tsx
@@ -4,7 +4,7 @@ import { MacbookScroll } from "@/components/ui/macbook-scroll";
 
 export default function Home() {
 	return (
-		<main className="flex flex-col justify-center items-center min-h-screen pt-24">
+		<main className="flex flex-col justify-center items-center min-h-screen pt-24 pb-10">
 			<HeroSection />
 			<MacbookScroll
 				title={<span className="text-white">Vote from your Home!</span>}
--- front/src/components/layout/Footer.tsx
+++ front/src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import { navigation } from "@/lib/const";
 
 export default function Footer() {
 	return (
-		<footer className="bg-black mt-10">
+		<footer className="bg-black ">
 			<div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
 				<nav
 					className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12"
