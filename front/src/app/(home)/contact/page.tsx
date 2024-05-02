e1562dddd5f87ae69e524563e85427f15bf35dd4
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-05-02 21:37:10 +0400

    [front] refactor: remove water n replace Inputs w Aceternity comp

--- front/src/app/(home)/contact/page.tsx
+++ front/src/app/(home)/contact/page.tsx
@@ -1,6 +1,16 @@
-import { IconBuilding, IconPhone, IconMail } from "@tabler/icons-react";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { IconPhone, IconMail } from "@tabler/icons-react";
+import { Label } from "@/components/ui/AceLabel";
+import { Input } from "@/components/ui/AceInput";
+import LabelInputContainer from "@/components/container/LabelInputContainer";
+import { useState } from "react";
 
 export default function Contact() {
+	const [email, setEmail] = useState("");
+	const [name, setName] = useState("");
+
 	return (
 		<div className="relative isolate bg-gray-900">
 			<div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
@@ -54,25 +64,10 @@ export default function Contact() {
 							Get in touch
 						</h2>
 						<p className="mt-6 text-lg leading-8 text-gray-300">
-							Proin volutpat consequat porttitor cras nullam gravida at. Orci
-							molestie a eu arcu. Sed ut tincidunt integer elementum id sem.
-							Arcu sed malesuada et magna.
+							Have question for us or feedback? Please fill out the form to
+							reach us.
 						</p>
 						<dl className="mt-10 space-y-4 text-base leading-7 text-gray-300">
-							<div className="flex gap-x-4">
-								<dt className="flex-none">
-									<span className="sr-only">Address</span>
-									<IconBuilding
-										className="h-7 w-6 text-gray-400"
-										aria-hidden="true"
-									/>
-								</dt>
-								<dd>
-									545 Mavis Island
-									<br />
-									Chicago, IL 99191
-								</dd>
-							</div>
 							<div className="flex gap-x-4">
 								<dt className="flex-none">
 									<span className="sr-only">Telephone</span>
@@ -83,7 +78,7 @@ export default function Contact() {
 								</dt>
 								<dd>
 									<a className="hover:text-white" href="tel:+1 (555) 234-5678">
-										+1 (555) 234-5678
+										+994 (50) 983 38 99
 									</a>
 								</dd>
 							</div>
@@ -100,7 +95,7 @@ export default function Contact() {
 										className="hover:text-white"
 										href="mailto:hello@example.com"
 									>
-										hello@example.com
+										mansur.mustafayev95@gmail.com
 									</a>
 								</dd>
 							</div>
@@ -114,73 +109,31 @@ export default function Contact() {
 				>
 					<div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
 						<div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-							<div>
-								<label
-									htmlFor="first-name"
-									className="block text-sm font-semibold leading-6 text-white"
-								>
-									First name
-								</label>
-								<div className="mt-2.5">
-									<input
+							<div className="sm:col-span-2">
+								<LabelInputContainer>
+									<Label htmlFor="name">Name</Label>
+									<Input
+										id="name"
+										placeholder="Your first name"
 										type="text"
-										name="first-name"
-										id="first-name"
-										autoComplete="given-name"
-										className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+										value={name}
+										onChange={(e) => setName(e.target.value)}
+										required
 									/>
-								</div>
-							</div>
-							<div>
-								<label
-									htmlFor="last-name"
-									className="block text-sm font-semibold leading-6 text-white"
-								>
-									Last name
-								</label>
-								<div className="mt-2.5">
-									<input
-										type="text"
-										name="last-name"
-										id="last-name"
-										autoComplete="family-name"
-										className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-									/>
-								</div>
+								</LabelInputContainer>
 							</div>
 							<div className="sm:col-span-2">
-								<label
-									htmlFor="email"
-									className="block text-sm font-semibold leading-6 text-white"
-								>
-									Email
-								</label>
-								<div className="mt-2.5">
-									<input
-										type="email"
-										name="email"
+								<LabelInputContainer>
+									<Label htmlFor="email">Email Address</Label>
+									<Input
 										id="email"
-										autoComplete="email"
-										className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+										placeholder="example@domain.com"
+										type="email"
+										value={email}
+										onChange={(e: any) => setEmail(e.target.value)}
+										required
 									/>
-								</div>
-							</div>
-							<div className="sm:col-span-2">
-								<label
-									htmlFor="phone-number"
-									className="block text-sm font-semibold leading-6 text-white"
-								>
-									Phone number
-								</label>
-								<div className="mt-2.5">
-									<input
-										type="tel"
-										name="phone-number"
-										id="phone-number"
-										autoComplete="tel"
-										className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-									/>
-								</div>
+								</LabelInputContainer>
 							</div>
 							<div className="sm:col-span-2">
 								<label
@@ -194,19 +147,20 @@ export default function Contact() {
 										name="message"
 										id="message"
 										rows={4}
-										className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+										className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-4 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
 										defaultValue={""}
 									/>
 								</div>
 							</div>
 						</div>
 						<div className="mt-8 flex justify-end">
-							<button
+							<Button
 								type="submit"
-								className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+								className="font-bold text-lg"
+								variant={"accent"}
 							>
 								Send message
-							</button>
+							</Button>
 						</div>
 					</div>
 				</form>
