56339bc881da1200ef973c0a3bbf11b4d91ff899
Medvezo <mansur.mustafayev@gmail.com>
2024-05-03 10:35:15 +0400

    [front] feat: map through them in FAQSection

--- front/src/components/container/FaqSection.tsx
+++ front/src/components/container/FaqSection.tsx
@@ -1,28 +1,48 @@
 import {
-	Accordion,
-	AccordionContent,
-	AccordionItem,
-	AccordionTrigger,
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
 } from "@/components/ui/accordion";
+import { web3Faqs, accountFaqs, paymentFaqs } from "@/lib/const";
+
 
 export default function FaqSection() {
-	return (
-		<section className="w-full  min-h-96 my-16">
-			<h3 className="text-3xl lg:text-4xl font-bold text-center">
-				Frequent Asked Questions
-			</h3>
-			<Accordion
-				type="single"
-				collapsible
-				className="max-w-md px-3 lg:px-0 mx-auto"
-			>
-				<AccordionItem value="item-1">
-					<AccordionTrigger>Is it accessible?</AccordionTrigger>
-					<AccordionContent>
-						Yes. It adheres to the WAI-ARIA design pattern.
-					</AccordionContent>
-				</AccordionItem>
-			</Accordion>
-		</section>
-	);
+    const faqCategories = [
+        {
+            title: "Web3",
+            faqs: web3Faqs
+        },
+        {
+            title: "Account",
+            faqs: accountFaqs
+        },
+        {
+            title: "Payment",
+            faqs: paymentFaqs
+        }
+    ];
+
+    return (
+        <section className="w-full my-16 px-5 md:px-10 ">
+            <h3 className="text-3xl lg:text-4xl font-bold text-center mb-10 text-primary-orange">
+                Frequently Asked Questions
+            </h3>
+            {faqCategories.map((category) => (
+                <div key={category.title} className="mb-8">
+                    <h4 className="text-2xl font-semibold mb-4  text-center text-amber-200">{category.title}</h4>
+                    <Accordion type="single" collapsible className="max-w-4xl mx-auto">
+                        {category.faqs.map((faq, index) => (
+                            <AccordionItem key={index} value={`item-${category.title.toLowerCase()}-${index}`}>
+                                <AccordionTrigger>{faq.question}</AccordionTrigger>
+                                <AccordionContent>
+                                    {faq.answer}
+                                </AccordionContent>
+                            </AccordionItem>
+                        ))}
+                    </Accordion>
+                </div>
+            ))}
+        </section>
+    );
 }
