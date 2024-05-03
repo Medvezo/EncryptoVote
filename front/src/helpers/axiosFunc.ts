bea8ffefd94b9d7061dd47643df49720685d82e4
GasimovNijat <nijatt.gasimov@gmail.com>
2024-05-03 11:41:12 +0400

    [front] fix: error 401 prevention handle on client side

--- front/src/helpers/axiosFunc.ts
+++ front/src/helpers/axiosFunc.ts
@@ -1,33 +1,37 @@
 import axios from "axios";
 
 type TUserRegisterData = {
-	name: string;
-	email: string;
-	password: string;
-    password_confirmation:string
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
 };
 
 // Function to handle login requests
-export const login =  async (email: string, password: string) => {
-    try {
-        const response = await axios.post('api/login', {
-            email: email,
-            password: password
-        });
-        return response.data;
-    } catch (error:any) {
-        throw error.response.data;
+export const login = async (email: string, password: string) => {
+  try {
+    const response = await axios.post("api/login", {
+      email: email,
+      password: password,
+    });
+    return response.data;
+  } catch (error: any) {
+    if (error.response && error.response.status !== 401) {
+      console.error("Registration failed", error);
+      throw error.response.data;
     }
+  }
 };
 
 // Function to handle registration requests
 export const register = async (UserRegisterData: TUserRegisterData) => {
-	try {
-		const response = await axios.post("api/signup", 
-            UserRegisterData
-        );
-		return response.data;
-	} catch (error: any) {
-		throw error.response.data;
-	}
+  try {
+    const response = await axios.post("api/signup", UserRegisterData);
+    return response.data;
+  } catch (error: any) {
+    if (error.response && error.response.status !== 401) {
+      console.error("Registration failed", error);
+      throw error.response.data;
+    }
+  }
 };
