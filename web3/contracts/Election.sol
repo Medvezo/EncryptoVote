c8f6982dacf05aaddd1ce83cd87dd37147d66e64
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-05-05 01:01:17 +0400

    [web3] feat: add getCandidates n getElligiblePolls to Election.sol

--- web3/contracts/Election.sol
+++ web3/contracts/Election.sol
@@ -8,14 +8,14 @@ pragma abicoder v2;
  */
 contract Ballot {
     struct Voter {
-        uint weight;
+        uint256 weight;
         bool voted; // whether person already voted or not
-        uint vote; // index of the voted proposal
+        uint256 vote; // index of the voted proposal
     }
 
     struct Candidate {
         string name;
-        uint voteCount;
+        uint256 voteCount;
     }
 
     struct Poll {
@@ -25,25 +25,25 @@ contract Ballot {
         bool isActive;
     }
 
-    mapping(uint => Poll) public polls;
-    uint public nextPollId;
+    mapping(uint256 => Poll) public polls;
+    uint256 public nextPollId;
 
     // Events
-    event PollCreated(uint pollId, string[] candidateNames);
-    event VoteCast(uint pollId, uint candidateIndex);
-    event PollStatusChanged(uint pollId, bool isActive);
+    event PollCreated(uint256 pollId, string[] candidateNames);
+    event VoteCast(uint256 pollId, uint256 candidateIndex);
+    event PollStatusChanged(uint256 pollId, bool isActive);
 
     // getters
     function getVoter(
-        uint pollId,
+        uint256 pollId,
         address voter
     ) public view returns (Voter memory) {
         return polls[pollId].voters[voter];
     }
 
     function getCandidate(
-        uint pollId,
-        uint candidateIndex
+        uint256 pollId,
+        uint256 candidateIndex
     ) public view returns (Candidate memory) {
         return polls[pollId].candidates[candidateIndex];
     }
@@ -53,7 +53,7 @@ contract Ballot {
         Poll storage p = polls[nextPollId++];
         p.chairperson = msg.sender;
         p.isActive = true;
-        for (uint i = 0; i < candidateNames.length; i++) {
+        for (uint256 i = 0; i < candidateNames.length; i++) {
             p.candidates.push(
                 Candidate({name: candidateNames[i], voteCount: 0})
             );
@@ -62,7 +62,7 @@ contract Ballot {
     }
 
     // Give a voter the right to vote on a specific poll
-    function giveRightToVote(uint pollId, address voter) public {
+    function giveRightToVote(uint256 pollId, address voter) public {
         require(
             msg.sender == polls[pollId].chairperson,
             "Only chairperson can give voting rights."
@@ -77,7 +77,7 @@ contract Ballot {
     }
 
     // Cast a vote on a specific poll
-    function vote(uint pollId, uint candidateIndex) public {
+    function vote(uint256 pollId, uint256 candidateIndex) public {
         Poll storage p = polls[pollId];
         require(p.isActive, "Poll is not active.");
         Voter storage sender = p.voters[msg.sender];
@@ -91,7 +91,7 @@ contract Ballot {
     }
 
     // End the voting period for a poll
-    function endPoll(uint pollId) public {
+    function endPoll(uint256 pollId) public {
         require(
             msg.sender == polls[pollId].chairperson,
             "Only chairperson can end the poll."
@@ -102,15 +102,48 @@ contract Ballot {
         emit PollStatusChanged(pollId, false);
     }
 
+    // Fetch polls where the user has voting rights
+    function getEligiblePolls(
+        address voter
+    ) public view returns (uint256[] memory) {
+        uint256[] memory eligiblePolls = new uint256[](nextPollId);
+        uint256 count = 0;
+        for (uint256 i = 0; i < nextPollId; i++) {
+            if (polls[i].voters[voter].weight > 0) {
+                eligiblePolls[count] = i;
+                count++;
+            }
+        }
+        return trimArray(eligiblePolls, count);
+    }
+
+    // Helper function to trim the array size
+    function trimArray(
+        uint256[] memory array,
+        uint256 length
+    ) private pure returns (uint256[] memory) {
+        uint256[] memory trimmedArray = new uint256[](length);
+        for (uint256 i = 0; i < length; i++) {
+            trimmedArray[i] = array[i];
+        }
+        return trimmedArray;
+    }
+
+    function getCandidates(
+        uint256 pollId
+    ) public view returns (Candidate[] memory) {
+        return polls[pollId].candidates;
+    }
+
     // Get the winning candidate for a poll
     function getWinningCandidate(
-        uint pollId
+        uint256 pollId
     ) public view returns (string memory winnerName) {
         Poll storage p = polls[pollId];
         require(!p.isActive, "Poll is still active.");
 
-        uint winningVoteCount = 0;
-        for (uint i = 0; i < p.candidates.length; i++) {
+        uint256 winningVoteCount = 0;
+        for (uint256 i = 0; i < p.candidates.length; i++) {
             if (p.candidates[i].voteCount > winningVoteCount) {
                 winningVoteCount = p.candidates[i].voteCount;
                 winnerName = p.candidates[i].name;
