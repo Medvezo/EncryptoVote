136314df32d692a481f8d88bad83bbc1ce2336bf
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-05-05 01:02:02 +0400

    [front] feat: add fetchAvaliablePolls in web3.ts

--- front/src/helpers/web3.ts
+++ front/src/helpers/web3.ts
@@ -1,9 +1,13 @@
 import { ethers } from "ethers";
+import { Poll, Candidate } from "@/components/sections/VotingSection";
 
 const contractABI = require("@/lib/contractABI.json");
 const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
-export async function createPoll(candidateNames: string[], signer: ethers.Signer) {
+export async function createPoll(
+	candidateNames: string[],
+	signer: ethers.Signer
+) {
 	const contract = new ethers.Contract(contractAddress, contractABI, signer);
 	const transaction = await contract.createPoll(candidateNames);
 	await transaction.wait();
@@ -59,4 +63,23 @@ export async function getCandidate(
 ) {
 	const contract = new ethers.Contract(contractAddress, contractABI, provider);
 	return await contract.getCandidate(pollId, candidateIndex);
+}
+
+// for fetching IDS
+export async function fetchAvailablePolls(
+	signer: ethers.Signer
+): Promise<Poll[]> {
+	const contract = new ethers.Contract(contractAddress, contractABI, signer);
+	const voterAddress = await signer.getAddress();
+	const pollIds = await contract.getEligiblePolls(voterAddress);
+
+	let polls: Poll[] = [];
+	for (let id of pollIds) {
+		const candidates = await contract.getCandidates(id); // Assuming getCandidates function exists
+		polls.push({
+			id,
+			candidates: candidates.map((c: any) => ({ name: c.name })),
+		});
+	}
+	return polls;
 }
