499985c230ec180fdb5618581fee4fc1a00cf689
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-05-05 01:03:23 +0400

    [front] refactor: delete unused in AppHeader

--- front/src/components/layout/AppHeader.tsx
+++ front/src/components/layout/AppHeader.tsx
@@ -8,7 +8,6 @@ import {
 	DropdownMenuSeparator,
 	DropdownMenuItem,
 } from "@/components/ui/dropdown-menu";
-import { Input } from "@/components/ui/input";
 import { Button } from "../ui/button";
 import Logo from "../common/Logo";
 
@@ -23,45 +22,12 @@ export default function AppHeader({ children }: { children: React.ReactNode }) {
 				>
 					Dashboard
 				</Link>
-				<Link
-					href="#"
-					className="text-muted-foreground transition-colors hover:text-foreground"
-				>
-					Orders
-				</Link>
-				<Link
-					href="#"
-					className="text-muted-foreground transition-colors hover:text-foreground"
-				>
-					Products
-				</Link>
-				<Link
-					href="#"
-					className="text-muted-foreground transition-colors hover:text-foreground"
-				>
-					Customers
-				</Link>
-				<Link
-					href="#"
-					className="text-foreground transition-colors hover:text-foreground"
-				>
-					Settings
-				</Link>
 			</nav>
 
 			{children}
 
-			<div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-				<form className="ml-auto flex-1 sm:flex-initial">
-					<div className="relative">
-						<Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-						<Input
-							type="search"
-							placeholder="Search products..."
-							className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
-						/>
-					</div>
-				</form>
+			<div className="flex w-full  items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+				<Button className="ml-auto" variant={"accent"}>+</Button>
 				<DropdownMenu>
 					<DropdownMenuTrigger asChild>
 						<Button variant="secondary" size="icon" className="rounded-full">
