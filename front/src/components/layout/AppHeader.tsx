c702ec2350626615616647bd762c3de42e21edd2
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-05-06 02:40:08 +0400

    [front] refactor: Make Universal NavBar for both group routes

--- front/src/components/layout/AppHeader.tsx
+++ front/src/components/layout/AppHeader.tsx
@@ -10,18 +10,15 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Button } from "../ui/button";
 import Logo from "../common/Logo";
+import { navigation } from "@/lib/const";
+import NavBar from "./NavBar";
 
 export default function AppHeader({ children }: { children: React.ReactNode }) {
 	return (
 		<header className="sticky top-0 flex h-20 items-center gap-4 border-b shadow-lg shadow-blue-950/50 bg-[#03001417] backdrop-blur-md z-40 px-4 lg:px-6 ">
 			<nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
 				<Logo size={70} noText href="/dashboard" />
-				<Link
-					href="#"
-					className="text-muted-foreground transition-colors hover:text-foreground"
-				>
-					Dashboard
-				</Link>
+				<NavBar navigation={navigation} />
 			</nav>
 
 			{children}
--- front/src/components/layout/NavBar.tsx
+++ front/src/components/layout/NavBar.tsx
@@ -1,29 +1,41 @@
-"use client";
-
-import { navigation } from "@/lib/const";
+"use client"
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-export default function NavBar() {
+interface NavItem {
+	name: string;
+	href: string;
+}
+
+interface Navigation {
+	[key: string]: NavItem[];
+}
+
+interface NavBarProps {
+	navigation: Navigation;
+}
+
+export default function NavBar({ navigation }: NavBarProps) {
 	const pathname = usePathname();
 
 	return (
-		<div className="hidden sm:text-sm  md:mr-16 md:flex md:justify-center md:items-center md:space-x-8 lg:text-lg ">
-			{navigation.main.map((item) => (
-				<div key={item.name}>
-					<Link
-						href={item.href}
-						// Apply conditional styles based on the current path
-						className={`inline-flex items-center border-b-2 px-1 pt-1 font-medium transition-all duration-300 ease-in-out ${
-							pathname === item.href
-								? "border-amber-500 text-white" // Styles if the URL matches
-								: "border-transparent text-gray-400 hover:border-gray-300 hover:text-gray-600" // Default styles
-						}`}
-					>
-						{item.name}
-					</Link>
-				</div>
-			))}
+		<div className="hidden sm:text-sm md:mr-16 md:flex md:justify-center md:items-center md:space-x-8 lg:text-lg">
+			{Object.values(navigation)
+				.flat()
+				.map((item) => (
+					<div key={item.name}>
+						<Link
+							href={item.href}
+							className={`inline-flex items-center border-b-2 px-1 pt-1 font-medium transition-all duration-300 ease-in-out ${
+								pathname === item.href
+									? "border-amber-500 text-white"
+									: "border-transparent text-gray-400 hover:border-gray-300 hover:text-gray-600"
+							}`}
+						>
+							{item.name}
+						</Link>
+					</div>
+				))}
 		</div>
 	);
 }
