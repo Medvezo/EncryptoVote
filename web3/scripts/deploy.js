4a122cf9c0c22377d6830eda32eb3023dfaf6cee
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-05-05 18:31:32 +0400

    [web3] feat: error prevention handling in deploy.js in /scripts

--- web3/scripts/deploy.js
+++ web3/scripts/deploy.js
@@ -2,6 +2,10 @@ const { ethers } = require("hardhat");
 
 async function main() {
 	const [deployer] = await ethers.getSigners();
+	if (!deployer) {
+		console.error("No deployer is available");
+		return;
+	}
 
 	console.log("Deploying contracts with the account:", deployer.address);
 	const accountBalance = await deployer.provider.getBalance(deployer.address);
