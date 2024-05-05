7dd88d16dbd476f28c55cfe257ec34fb87715791
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-05-06 01:01:17 +0400

    [front] feat: accept event n re-rerender n style w Card ManageCreatedPoll

--- front/src/components/sections/ManageCreatedPollSection.tsx
+++ front/src/components/sections/ManageCreatedPollSection.tsx
@@ -5,21 +5,35 @@ import { fetchAllPolls, endPoll, getWinningCandidate } from "@/helpers/web3";
 import { Button } from "../ui/button";
 import GrantVoteRightsModal from "../forms/GrantVoteRightsModal";
 import { Poll } from "./VotingSection";
-
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
 export default function ManageCreatedPollSection() {
 	const { signer } = useWallet();
-    const [polls, setPolls] = useState<Poll[]>([]);
+	const [polls, setPolls] = useState<Poll[]>([]);
 
 	useEffect(() => {
-        const fetchPolls = async () => {
-            if (signer) {
-                const retrievedPolls = await fetchAllPolls(signer);
-                setPolls(retrievedPolls);
-            }
-        };
-        fetchPolls();
-    }, [signer]);
-    
+		const fetchPolls = async () => {
+			if (signer) {
+				const retrievedPolls = await fetchAllPolls(signer);
+				setPolls(retrievedPolls);
+			}
+		};
+
+		fetchPolls();
+
+		const handlePollCreated = () => fetchPolls(); // handler that refetches polls
+
+		window.addEventListener("poll-created", handlePollCreated);
+		return () => {
+			window.removeEventListener("poll-created", handlePollCreated);
+		};
+	}, [signer]);
 
 	const handleEndPoll = async (pollId: number) => {
 		if (signer) {
@@ -43,23 +57,43 @@ export default function ManageCreatedPollSection() {
 	};
 
 	return (
-		<div>
+		<section className="flex gap-10 flex-wrap">
 			{polls.length > 0 ? (
 				polls.map((poll: Poll) => (
-					<div key={poll.id} className="poll-card">
-						<h2>Poll ID: {poll.id}</h2>
-						<div>
+					<Card
+						key={poll.id}
+						className="poll-card max-w-md min-w-80 lg:min-w-96"
+					>
+						<CardHeader>
+							<CardTitle className="text-xl font-bold text-amber-500 text-center">
+								Poll ID: {poll.id}
+							</CardTitle>
+							<CardDescription>Canditates:</CardDescription>
+						</CardHeader>
+						<CardContent className="font-bold ">
+							{poll.candidates.map((candidate, index) => (
+								<div key={index}>{candidate.name}</div>
+							))}
+						</CardContent>
+						<CardFooter className="flex flex-col gap-5">
 							<GrantVoteRightsModal pollId={poll.id} />
-							<Button onClick={() => handleEndPoll(poll.id)}>End Voting</Button>
-							<Button onClick={() => handleGetWinner(poll.id)}>
-								Get Winner
-							</Button>
-						</div>
-					</div>
+							<div className="col-span flex gap-10">
+								<Button
+									variant={"destructive"}
+									onClick={() => handleEndPoll(poll.id)}
+								>
+									End Voting
+								</Button>
+								<Button onClick={() => handleGetWinner(poll.id)}>
+									Get Winner
+								</Button>
+							</div>
+						</CardFooter>
+					</Card>
 				))
 			) : (
 				<p>No polls available to manage.</p>
 			)}
-		</div>
+		</section>
 	);
 }
