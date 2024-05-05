c438d94a60e8957298e6bc0bb3b978f864d473c8
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-05-06 03:55:51 +0400

    [front] style: apply  bg color for all pages

--- front/src/components/sections/BentoGridSection.tsx
+++ front/src/components/sections/BentoGridSection.tsx
@@ -74,7 +74,6 @@ export default function BentoGridSection() {
 					title={item.title}
 					description={item.description}
 					header={item.header}
-					icon={item.icon}
 					className={i === 3 || i === 6 ? "md:col-span-2" : ""}
 				/>
 			))}
